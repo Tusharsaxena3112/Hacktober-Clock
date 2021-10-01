b12de0d75c5247c666abb00a80505b633f2bae6c
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-10-01 14:20:04 +0530

    changed time to 12 hour format

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function update(){
     let time=new Date();
     let hrs=time.getHours();
+    suffix = hrs >= 12 ? "PM":"AM";
+    hrs=((hrs % 12) || 12);
     document.getElementById('hours').innerHTML=hrs;
     let min=time.getMinutes();
     document.getElementById('minutes').innerHTML=min;
-    let sec=time.getSeconds();
+    let sec=(time.getSeconds())+' '+suffix;
     document.getElementById('seconds').innerHTML=sec;
 }
 setInterval(update,1000);
