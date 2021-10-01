2085682590fbe46eae41400b6beba26f0444f3c3
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-10-01 15:30:21 +0530

    changed time to 12 hour format (#2)
    * changed time to 12 hour format
    
    * declared variables
    
    * reversed the changes
    
    * declared variables

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function update(){
     let time=new Date();
     let hrs=time.getHours();
+    let suffix = hrs >= 12 ? "PM":"AM";
+    hrs=((hrs % 12) || 12);
     document.getElementById('hours').innerHTML=hrs;
     let min=time.getMinutes();
     document.getElementById('minutes').innerHTML=min;
-    let sec=time.getSeconds();
+    let sec=(time.getSeconds())+' '+suffix;
     document.getElementById('seconds').innerHTML=sec;
     let date=time.getUTCDate();
     let month=time.getUTCMonth();
