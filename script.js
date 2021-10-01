faec433f1b43b1802fbc0dd0100da2042a79aee0
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-10-01 15:18:10 +0530

    reversed the changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function update(){
     let time=new Date();
     let hrs=time.getHours();
-    var suffix = hrs >= 12 ? "PM":"AM";
-    var hrs=((hrs % 12) || 12);
+    suffix = hrs >= 12 ? "PM":"AM";
+    hrs=((hrs % 12) || 12);
     document.getElementById('hours').innerHTML=hrs;
     let min=time.getMinutes();
     document.getElementById('minutes').innerHTML=min;
