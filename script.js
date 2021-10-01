9138d9486238c29122e2a6e126d22d8118d06cd9
Divyanshi-Bansal <54770459+Divyanshi-Bansal@users.noreply.github.com>
2021-10-01 15:20:27 +0530

    add date, update time interval (#1)

--- script.js
+++ script.js
@@ -6,5 +6,27 @@ function update(){
     document.getElementById('minutes').innerHTML=min;
     let sec=time.getSeconds();
     document.getElementById('seconds').innerHTML=sec;
+    let date=time.getUTCDate();
+    let month=time.getUTCMonth();
+    let year=time.getFullYear();
+    document.getElementById('date').innerHTML=date+' '+monthName(month+1)+', '+year;
 }
-setInterval(update,1000);
+
+function monthName(mNumber){
+    var mName = new Array(12);
+    mName[1] = 'Jan';
+    mName[2] = 'Feb';
+    mName[3] = 'March;';
+    mName[4] = 'April';
+    mName[5] = 'May';
+    mName[6] = 'June';
+    mName[7] = 'July';
+    mName[8] = 'Aug';
+    mName[9] = 'Sep';
+    mName[10] = 'Oct';
+    mName[11] = 'Nov';
+    mName[12] = 'Dec';
+    return mName[mNumber];
+}
+
+setInterval(update,500);
